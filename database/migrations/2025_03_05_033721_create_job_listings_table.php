66574634f8ee98893ae4d99f8bd3ebd3b16bc337
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-03-04 21:44:41 -0600

    Day 8 Complete

---
+++ database/migrations/2025_03_05_033721_create_job_listings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// Created using the `php artisan make:migration create_job_listings_table` command
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_listings', function (Blueprint $table) {
+            $table->id();
+            // Created new table using `php artisan migrate` command.
+            // Press Ctrl + Shift + R to refresh TablePlus
+            $table->string('title');
+            $table->string('salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_listings');
+    }
+};
