28071f5a26bf5cc1c016b81ef1cae251b03d5f76
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-03-29 19:06:28 -0500

    Completed Days 16-17

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,40 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
-
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.show', ['job' => $job]);
+});
+
 Route::post('/jobs', function () {
+
     request()->validate([
-        'employer_id' => ['required', 'exists:employers'],
-        'title'       => ['required'],
-        'salary'      => ['required'],
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
     ]);
+
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
 
 Route::get('/jobs-test', function () {
@@ -28,14 +49,4 @@ Route::get('/jobs-test', function () {
 //    dd($jobs);
 //    dd($jobs[0]);
     dd($jobs[0]->title . ' ' . $jobs[0]->salary);
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('job', ['job' => $job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
 });
