19229ee578395007983fcd5a6721c98aca25d83f
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-04-13 12:59:23 -0500

    Day 23

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-// Created using the `php artisan make:model Employer -m -f` command
-// This command created the Model, Migration, and Factory
 class Employer extends Model
 {
     use HasFactory;
@@ -14,5 +13,10 @@ class Employer extends Model
     public function jobs()
     {
         return $this->hasMany(Job::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -9,6 +9,9 @@
         This job pays {{ $job->salary }} per year.
     </p>
 
-    <p class="mt-6">
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    @can('edit', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        </p>
+    @endcan
 </x-layout>
